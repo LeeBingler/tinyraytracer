b551ea16e008516759f776f8b59ea7e6ff13eb8a
Lee <binglerlee@gmail.com>
2026-09-22 06:40:02 +0200

    feat: add geometry.h + update CMakeList.txt

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@ void render() {
   std::ofstream ofs; // save the framebuffer to file
   ofs.open("./out.ppm");
   ofs << "P6\n" << width << " " << height << "\n255\n";
+
   for (size_t i = 0; i < height * width; ++i) {
     for (size_t j = 0; j < 3; j++) {
       ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
